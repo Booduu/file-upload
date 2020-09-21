fbbbd3467f30ca549be143b40e667fac7ec7cb53
Mathias Payen <mathias.payen@gmail.com>
2020-09-21 15:13:32 +0200

    material test

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
 import { storage } from '../../conf/firebaseApi';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+
 
 const FirebaseFileUpload = () => {
 
     const [image, setImage] = useState(null);
     const [ url, setUrl ] = useState("");
     const [ progress, setProgress ] = useState(0);
+    const [ disabledButton, setdisabledButton ] = useState(true)
 
     const handleChange= (e) => {
         if(e.target.files[0]) {
             setImage(e.target.files[0]);
+            setdisabledButton(false)
         }
     }
 
     const handleUpload = () => {
+
         const upload = storage.ref(`images/${image.name}`).put(image);
+
         upload.on(
             "state_changed",
             snapshot => {
@@ -33,25 +42,26 @@ const FirebaseFileUpload = () => {
                     .getDownloadURL()
                     .then(url => {
                         setUrl(url)
+                        console.log(image)
                     })
             }
         )
     }
 
-    console.log('image:', image );
+  
 
     return (
-        <div>
-            <progress value={ progress } max="100"/>
-            <br />
-            <input onChange={ handleChange } type='file'/>
-            <button onClick={ handleUpload } type="button">Upload</button>
-            <br />
-            { url && (
-                <img src={ url } width="200px"/>
-            ) }
-        </div>
+        <Container maxWidth="xl" >
+            <Box style={{ display: "flex", flexDirection: "column", alignItems: "center", height: "200px" }}>
+                <Input onChange={ handleChange } type='file' style={{ width:"50%",  }}/>
+                <Button onClick={ handleUpload } style={{ width:"50%" }} variant="contained" color="secondary" disabled={ disabledButton }>Upload</Button>
+                <progress value={ progress } max="100"/>
+                { url && (
+                    <img src={ url } alt={ image.name }  width="200px"/>
+                ) }
+            </Box>
+        </Container>
     )
 }
 
-export default FirebaseFileUpload;
+export default (FirebaseFileUpload);
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,1 @@
-export { default as FirebaseFileUpload } from './form/Form'
+export { default as FirebaseFileUpload } from './form/Form';
