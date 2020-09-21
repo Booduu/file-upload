a1196d55ea6b104090bee0981316c1327ba31534
Mathias Payen <mathias.payen@gmail.com>
2020-09-21 12:04:04 +0200

    upload file on firebase & recover url

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -17,7 +17,12 @@ const FirebaseFileUpload = () => {
         const upload = storage.ref(`images/${image.name}`).put(image);
         upload.on(
             "state_changed",
-            snapshot => {},
+            snapshot => {
+                const progress = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                );
+                setProgress(progress)
+            },
             error => {
               console.log(error);
             },
@@ -37,9 +42,15 @@ const FirebaseFileUpload = () => {
 
     return (
         <div>
+            <progress value={ progress } max="100"/>
+            <br />
             <input onChange={ handleChange } type='file'/>
             <button onClick={ handleUpload } type="button">Upload</button>
-            { url }
+            <br />
+            { url && (
+                <img src={ url } width="200px"/>
+            ) }
+            
         </div>
     )
 }
