e68dd018d20331b854b72903475e2f24956ae2ef
Mathias PAYEN <62614636+Booduu@users.noreply.github.com>
2020-11-10 13:10:24 +0100

    Update firebaseApi.js

--- src/conf/firebaseApi.js
+++ src/conf/firebaseApi.js
@@ -4,14 +4,14 @@ import "firebase/storage";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyADf9PjJEs9ExwEXM6xwvb2iWY4E6o30UA",
+    apiKey: "<apikey>",
     authDomain: "upload-db9bd.firebaseapp.com",
     databaseURL: "https://upload-db9bd.firebaseio.com",
     projectId: "upload-db9bd",
     storageBucket: "upload-db9bd.appspot.com",
     messagingSenderId: "354225422448",
-    appId: "1:354225422448:web:3f09ea03d9dc9aad2574b5",
-    measurementId: "G-77DX4VPD6K"
+    appId: "<apiid>",
+    measurementId: "<measurementId>"
   };
 
 
